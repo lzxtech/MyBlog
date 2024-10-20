e20765996e5a55cc919b3abfecc8b9c7f8da7147
liuzixuan <lzxtech@3398263811@qq.com>
2024-10-20 22:38:01 +0800

    Site updated: 2024-10-20 22:38:01

--- js/fomal.js
+++ js/fomal.js
@@ -54,7 +54,7 @@ function tonav() {
     position = scroll;
   });
   //修复没有弄右键菜单的童鞋无法回顶部的问题
-  document.getElementById("page-name").innerText = document.title.split(" | 🎊ATRI🎊")[0];
+  document.getElementById("page-name").innerText = document.title.split(" | 💝鸢尾花💝")[0];
 }
 
 function scrollToTop() {
@@ -1113,13 +1113,13 @@ function changeMouseMode() {
 var now1 = new Date();
 
 function createtime1() {
-  var grt = new Date("08/09/2022 00:00:00"); //此处修改你的建站时间或者网站上线时间
+  var grt = new Date("20/10/2024 00:00:00"); //此处修改你的建站时间或者网站上线时间
   now1.setTime(now1.getTime() + 250);
   var days = (now1 - grt) / 1000 / 60 / 60 / 24;
   var dnum = Math.floor(days);
 
   var ascll = [
-    `欢迎来到 🎊ATRI🎊の小家!`,
+    `欢迎来到 🎊鸢尾花🎊の小家!`,
     `你若安好💝，便是晴天☀️`,
     `
         
@@ -1172,7 +1172,7 @@ function createtime2() {
   setTimeout(
     console.warn.bind(
       console,
-      "%c ⚡ Powered by 🎊ATRI🎊 %c 你正在访问🎊ATRI🎊の小家",
+      "%c ⚡ Powered by 💝鸢尾花💝 %c 你正在访问💝鸢尾花💝の小家",
       "color:white; background-color:#f0ad4e",
       ""
     )
@@ -1282,8 +1282,8 @@ function share_() {
   try {
     // 截取标题
     var title = document.title;
-    var subTitle = title.endsWith("| 🎊ATRI🎊") ? title.substring(0, title.length - 14) : title;
-    navigator.clipboard.writeText('🎊ATRI🎊的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
+    var subTitle = title.endsWith("| 💝鸢尾花💝") ? title.substring(0, title.length - 14) : title;
+    navigator.clipboard.writeText('💝鸢尾花💝的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
     new Vue({
       data: function () {
         this.$notify({
