881bafc0cb51619411be891fdbff763ce11b0cc2
liuzixuan <lzxtech@3398263811@qq.com>
2024-10-24 22:10:23 +0800

    Site updated: 2024-10-24 22:10:22

--- js/fomal.js
+++ js/fomal.js
@@ -292,7 +292,7 @@ function showWelcome() {
 }
 window.onload = showWelcome;
 // 如果使用了pjax在加上下面这行代码
-// document.addEventListener('pjax:complete', showWelcome);
+document.addEventListener('pjax:complete', showWelcome);
 
 /* 欢迎信息 end */
 
